b47853db9930503feaa19298b158b4a1931e4eaa
contigen <69436315+contigen@users.noreply.github.com>
2023-08-21 18:12:43 +0100

    Remove NextUI Button component

--- src/app/ip/page.tsx
+++ src/app/ip/page.tsx
@@ -1,6 +1,3 @@
-import { Button } from "@nextui-org/button"
-// import { Textarea } from "@nextui-org/react"
-
 export default async function IP() {
   const res = await fetch(`http://localhost:3000/api/ip`, {
     body: `hello, other side`,
@@ -8,7 +5,7 @@ export default async function IP() {
   const result = await res.json()
   return (
     <>
-      <Button>Check IP</Button>
+      <button>Check IP</button>
       <div className='border rounded-3xl shadow p-4'>
         {result.clientIP === `` ? `No IP` : result.clientIP}
       </div>
