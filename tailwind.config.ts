454ea6193093d3db96c9cab759d6458feb3a0de0
contigen <69436315+contigen@users.noreply.github.com>
2023-08-22 04:56:20 +0100

    (Re)updated tw config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const { nextui } = require('@nextui-org/react')
+
 module.exports = {
   content: [
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {},
+  darkMode: 'class',
+  plugins: [nextui()],
 }
