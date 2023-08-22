bc9ccc40f6b2f6e57f8cc2c76cfa50d359ed2688
contigen <69436315+contigen@users.noreply.github.com>
2023-08-22 04:51:07 +0100

    Refactor code

--- src/app/api/ip/route.ts
+++ src/app/api/ip/route.ts
@@ -2,11 +2,6 @@ import { headers } from "next/headers"
 import { NextResponse } from "next/server"
 
 export async function GET(req: Request) {
-  // const clientIP = headers().get(`x-forwarded-for`)
-  // return NextResponse.json({ clientIP })
-  const allowedIP = process.env.NODE_ENV === `production` ? "prodip" : "::1"
-  const data = await req.json()
-  const headersList = headers()
-  const clientIP = headersList.get("x-forwarded-for")
-  console.log(clientIP)
+  const clientIP = headers().get(`x-forwarded-for`)
+  return NextResponse.json({ clientIP })
 }
