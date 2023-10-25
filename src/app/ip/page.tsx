d0526dd48c4f6a6d8031584bb180d05a272861be
contigen <69436315+contigen@users.noreply.github.com>
2023-10-25 20:55:38 +0100

    Update  Button component URL

--- src/app/ip/page.tsx
+++ src/app/ip/page.tsx
@@ -1,5 +1,5 @@
 import { BASE_URL } from '@/base-url'
-import { Button } from '@nextui-org/react'
+import { Button } from '@nextui-org/button'
 
 export default async function IP() {
   const res = await fetch(`${BASE_URL}/api/ip`)
