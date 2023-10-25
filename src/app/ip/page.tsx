042d0f4ae1cd05048015f004588d8d54f1ca684e
contigen <69436315+contigen@users.noreply.github.com>
2023-10-25 21:21:02 +0100

    Add host headers field

--- src/app/ip/page.tsx
+++ src/app/ip/page.tsx
@@ -1,13 +1,16 @@
-import { BASE_URL } from '@/base-url'
+import { headers } from 'next/headers'
+// import { BASE_URL } from '@/base-url'
 import { Button } from '@nextui-org/button'
 
 export default async function IP() {
-  const res = await fetch(`${BASE_URL}/api/ip`)
-  const result = await res.json()
+  const host = headers().get('host')
+  // const res = await fetch(`${BASE_URL}/api/ip`)
+  // const result = await res.json()
   return (
     <>
+      <h1>{host}</h1>
       <Button>Check IP</Button>
-      <h2>{result.clientIP === `` ? `No IP` : result.clientIP}</h2>
+      {/* <h2>{result.clientIP === `` ? `No IP` : result.clientIP}</h2> */}
     </>
   )
 }
