8440e77bc98f09c553c4b7abf4070f1a6670e236
contigen <69436315+contigen@users.noreply.github.com>
2023-10-26 15:50:08 +0100

    Update base URL

--- src/base-url.ts
+++ src/base-url.ts
@@ -1,2 +1,3 @@
 const { VERCEL_URL: vURL, PORT } = process.env
-export const BASE_URL = PORT ? `http://localhost:${PORT}` : vURL
+
+export const BASE_URL = PORT ? `http://localhost:${PORT}` : `https://${vURL}`
