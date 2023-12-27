4beaf23f4a240e018626f630f89716738b54f9f9
contigen <69436315+contigen@users.noreply.github.com>
2023-12-27 14:59:08 +0100

    Refactor code

--- src/ui/ip.tsx
+++ src/ui/ip.tsx
@@ -1,13 +1,8 @@
 import { BASE_URL } from '@/base-url'
 
-async function getIPAddress() {
+export async function IPComponent() {
   const IP_ENDPOINT = `${BASE_URL}/api/ip`
   const res = await fetch(IP_ENDPOINT)
   const { clientIP: IPAddress } = await res.json()
-  return IPAddress
-}
-
-export async function IPComponent() {
-  const IPAddress = await getIPAddress()
   return <h2>{IPAddress ?? `No IP Address`}</h2>
 }
