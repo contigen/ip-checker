3a250da18991e0ac29dac33f7a264c1c68d31e0f
contigen <69436315+contigen@users.noreply.github.com>
2023-12-27 19:36:40 +0100

    Update route handler

--- src/app/api/ip/route.ts
+++ src/app/api/ip/route.ts
@@ -1,15 +1,14 @@
 import { headers } from 'next/headers'
-import { NextResponse } from 'next/server'
 import { geolocation, ipAddress } from '@vercel/edge'
 
 export async function GET(req: Request) {
   const clientIP = headers().get(`x-forwarded-for`)
   const { city, country, countryRegion, latitude, longitude, flag, region } =
     geolocation(req)
-  const ResponseHeaders = new Headers()
+  const ResponseHeaders = new Headers(req.headers)
   ResponseHeaders.set(`Content-Type`, `application/json`)
   const ip = ipAddress(req)
-  return NextResponse.json(
+  return Response.json(
     {
       city,
       country,
