7631b4cdb30f395856ed497090c7fa93f2cf2663
contigen <69436315+contigen@users.noreply.github.com>
2023-12-27 16:50:25 +0100

    Remove console.log

--- src/app/api/ip/route.ts
+++ src/app/api/ip/route.ts
@@ -4,7 +4,6 @@ import { geolocation, ipAddress } from '@vercel/edge'
 
 export async function GET(req: Request) {
   const clientIP = headers().get(`x-forwarded-for`)
-  console.log(headers().get(`x-real-ip`))
   const { city, country, countryRegion, latitude, longitude, flag, region } =
     geolocation(req)
   return NextResponse.json({
