cde0c594825df943ffc678a5fa7b1362d5bc1511
contigen <69436315+contigen@users.noreply.github.com>
2023-12-27 16:50:48 +0100

    Remove  headers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,18 @@
-import { headers } from 'next/headers'
 import { Suspense } from 'react'
 import { IPComponent } from '@/ui/ip'
 import { Spinner } from '@/spinner'
+import { BASE_URL } from './base-url'
 
 export default function Home() {
-  const referer = headers().get(`referer`)
-
   return (
     <section>
       <hgroup>
         <h1>Check your IP address</h1>
-        <h1 style={{ fontFamily: `Samsung Sharp Sans` }}>{referer}</h1>
+        <h1 style={{ fontFamily: `Samsung Sharp Sans` }}>{BASE_URL}</h1>
       </hgroup>
-      <div>
-        <Suspense fallback={Spinner}>
-          <IPComponent />
-        </Suspense>
-      </div>
+      <Suspense fallback={Spinner}>
+        <IPComponent />
+      </Suspense>
     </section>
   )
 }
